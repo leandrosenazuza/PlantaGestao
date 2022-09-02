1e102da118adabdc8b44ad067431fbb9f9b501d0
Leandro Sena Zuza <leandrosenazuza@gmail.com>
2022-09-01 22:28:56 -0300

    primeiro commit

---
+++ src/main/java/br/com/leandrosenazua/GerenciamentoPlanta/GerenciamentoPlantaApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrosenazua.GerenciamentoPlanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoPlantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoPlantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandrosenazua/GerenciamentoPlanta/GerenciamentoPlantaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrosenazua.GerenciamentoPlanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPlantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
